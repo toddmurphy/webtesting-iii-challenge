a16d57b58dbc78acd7488ca45b8631e6878b794a
toddmurphy <toddmurphy93@gmail.com>
2019-12-16 16:58:32 -0400

    got one test app to work that app is displaying

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,10 @@
 // Test away
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react';
+
+import Dashboard from './Dashboard';
+
+test('Dashboard is displaying correctly', () => {
+    expect(render(<Dashboard />)).toMatchSnapshot()  
+})
+
