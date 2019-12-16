4a1123d9ff3e0331ae83031e6fa92ac4b1bbb067
toddmurphy <toddmurphy93@gmail.com>
2019-12-16 19:09:34 -0400

    added dashboard test for Locked

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -3,8 +3,15 @@ import React from 'react';
 import {render, fireEvent} from '@testing-library/react';
 
 import Dashboard from './Dashboard';
+import Controls from '../controls/Controls';
 
 test('Dashboard component displays correctly', () => {
     render(<Dashboard />)
 })
 
+test('When "Lock Gate" button is clicked Unlocked green text changes text to "Locked"', () => {
+    const {getByText} = render(<Dashboard />)
+    const button = getByText(/^Lock Gate$/i)
+    fireEvent.click(button)
+    getByText(/^Locked$/)
+})
