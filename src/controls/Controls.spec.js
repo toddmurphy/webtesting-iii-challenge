70902b272470772cdb383950d0e63098ea00438b
toddmurphy <toddmurphy93@gmail.com>
2019-12-16 19:47:22 -0400

    added disabled button to Controls test

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,6 +1,7 @@
 // Test away!
 import React from 'react';
 import {render, fireEvent} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 import Controls from './Controls';
 import Dashboard from '../dashboard/Dashboard';
@@ -14,7 +15,9 @@ test('Controls component is displaying correctly', () => {
 test('Click unlock gate button that changes to unlocked green color', () => {
     const {getByText} = render(<Dashboard />)
     const button = getByText(/^Lock Gate$/i)
+    const button2 = getByText(/^open gate$/i)
     fireEvent.click(button)
     getByText(/^Unlock Gate$/i)
+    expect(button2).toBeDisabled()
 })
 
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -11,7 +11,16 @@ test('Dashboard component displays correctly', () => {
 
 test('When "Lock Gate" button is clicked Unlocked green text changes text to "Locked"', () => {
     const {getByText} = render(<Dashboard />)
-    const button = getByText(/^Lock Gate$/i)
+    const button = getByText(/^lock gate$/i)
     fireEvent.click(button)
-    getByText(/^Locked$/)
+    getByText(/^locked$/i)
+    getByText(/^unlock gate$/i)
+})
+
+test('When "Open Gate" button is clicked Closed red text changes to Open text and Close Gate button', () => {
+    const {getByText} = render(<Dashboard />)
+    const button = getByText(/^open gate$/i)
+    fireEvent.click(button)
+    getByText(/^open$/i)
+    getByText(/^close gate$/i)
 })
