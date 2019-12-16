db18a8bf6735540c9b624e02dbef18afb6b8f444
toddmurphy <toddmurphy93@gmail.com>
2019-12-16 18:42:34 -0400

    some control test working

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -3,8 +3,18 @@ import React from 'react';
 import {render, fireEvent} from '@testing-library/react';
 
 import Controls from './Controls';
+import Dashboard from '../dashboard/Dashboard';
 
 test('Controls component is displaying correctly', () => {
     render(<Controls />)
 
 })
+
+
+test('Click unlock gate button that changes to unlocked green color', () => {
+    const {getByText} = render(<Dashboard />)
+    const button = getByText(/^Lock Gate$/i)
+    fireEvent.click(button)
+    getByText(/^Unlock Gate$/i)
+})
+
